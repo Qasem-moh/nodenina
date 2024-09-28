759c1f385beb2fb3f3a35655b1a1b372233d3b73
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-09-28 22:50:07 +0300

    nodejs

--- app.js
+++ app.js
@@ -2,27 +2,38 @@ const express=require("express")
 const app=express()
 
 //register view engin
-app.set('view engin','ejs')
+app.set('view engine','ejs')
 
 //root api
 app.get("/",(req,res)=>{
-    res.sendFile('./views/home.html',{root:__dirname})
+    // res.sendFile('./views/home.html',{root:__dirname})
+    const blogs=[
+        {title: 'Qasem',snippet:'lorem ipsum dolor sit amet consectetur'},
+        {title: 'Ahmad',snippet:'lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mohammad',snippet:'lorem ipsum dolor sit amet consectetur'},
+    ]
+    res.render('index',{blogs})
 })
 
 //about endpoint
 app.get('/about',(req,res)=>{
     // res.sendFile("./views/about.html",{root:__dirname})
-    res.render('index')
+    res.render('about')
+})
+
+app.get('/blogs/create',(req,res)=>{
+    res.render('newBlog')
 })
 
 //redirect endpoint to about
-app.get('/about-us',(req,res)=>{
-    res.redirect('/about')
-})
+// app.get('/about-us',(req,res)=>{
+//     res.redirect('/about')
+// })
 
 //use  404 page
 app.use((req,res)=>{
-    res.status(401).sendFile('./views/404.html',{root:__dirname})
+    // res.status(401).sendFile('./views/404.html',{root:__dirname})
+    res.status(404).render('404')
 })
 
 //listening server
